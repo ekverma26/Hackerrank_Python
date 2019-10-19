d2877973678fe93c964b129a220604990f4ba44c
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-10-19 15:55:56 +0530

    Create substring.py

---
+++ substring.py
@@ -0,0 +1,22 @@
+'''In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
+
+NOTE: String letters are case-sensitive.
+
+Input Format: The first line of input contains the original string. The next line contains the substring.
+Output Format: Output the integer number indicating the total number of occurrences of the substring in the original string.'''
+
+
+def count_substring(string, sub_string):
+    count=0
+    for i in range(0,len(string)):
+        if string[i]==sub_string[0]:
+            if string[i:i+len(sub_string)]==sub_string:
+                count=count+1
+    return count
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
