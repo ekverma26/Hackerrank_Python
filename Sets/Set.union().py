7b6eea52af93a1715e068354c85cc794632bccc6
Ekta Verma <46107362+ekverma26@users.noreply.github.com>
2019-11-08 19:47:11 +0530

    Create Set.union().py

---
+++ Sets/Set.union().py
@@ -0,0 +1,28 @@
+# Task
+# Students of District College have a subscription to English and French newspapers. Some students have subscribed to only the English
+# newspaper, some have subscribed to only the French newspaper, and some have subscribed to both newspapers.
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the
+# French newspaper. Your task is to find the total number of students who have subscribed to only English newspapers.
+
+# Input Format
+# The first line contains the number of students who have subscribed to the English newspaper. 
+# The second line contains the space separated list of student roll numbers who have subscribed to the English newspaper.
+# The third line contains the number of students who have subscribed to the French newspaper. 
+# The fourth line contains the space separated list of student roll numbers who have subscribed to the French newspaper.
+
+# Constraints
+# 0 < Total number of students in college < 1000
+
+# Output Format
+# Output the total number of students who are subscribed to the English newspaper only.
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n=int(input())
+s1 = set(map(int, input().split()))
+b=int(input())
+s2 = set(map(int, input().split()))
+s3=set()
+s3=s1.union(s2)
+print(len(s3))
